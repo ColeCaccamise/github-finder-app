848f7f967f1462117da4b0b4edacb913bf410b34
Cole Caccamise <colecaccamise@gmail.com>
2022-02-03 19:23:12 -0500

    most recent version

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -12,6 +12,8 @@ console.log('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')
 export const GithubProvider = ({ children }) => {
     const initialState = {
         users: [],
+        user: {},
+        repos: [],
         loading: false
     }
 
@@ -23,9 +25,9 @@ export const GithubProvider = ({ children }) => {
 
         const params = new URLSearchParams({
             q: text
-        })
+        });
 
-        console.log(`${GITHUB_URL}/search/users?${params}`)
+        console.log(`${GITHUB_URL}/search/users?${params}`);
 
         const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
             headers: {
@@ -41,6 +43,50 @@ export const GithubProvider = ({ children }) => {
         })
     }
 
+    // Get user repos
+    const getUserRepos = async (login) => {
+        setLoading();
+
+        const params = new URLSearchParams({
+            sort: 'created',
+            per_page: 10
+        })
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`
+            }
+        });
+
+        const data = await response.json();
+
+        dispatch({
+            type: 'GET_REPOS',
+            payload: data
+        })
+    }
+        // Get single user
+        const getUser = async (login) => {
+            setLoading();
+    
+            const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+                headers: {
+                    Authorization: `token ${GITHUB_TOKEN}`
+                }
+            });
+
+            if(response.status === 404) {
+                window.location = '/notfound';
+            } else {
+                const data = await response.json(); // single user coming back from the response
+    
+                dispatch({
+                    type: 'GET_USER',
+                    payload: data
+                })
+            }
+        }
+
     // Set loading 
     const setLoading = () => dispatch({type: 'SET_LOADING'})
 
@@ -52,8 +98,12 @@ export const GithubProvider = ({ children }) => {
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
+        user: state.user,
+        repos: state.repos,
         searchUsers,
-        clearUsers
+        clearUsers,
+        getUser,
+        getUserRepos
     }}>
         {children}
     </GithubContext.Provider>
